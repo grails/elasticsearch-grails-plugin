42f8b9ba7393fcdae77fd5c7d5676f70dc309909
Mihael Koep <mihael@koep.family>
2026-06-15 20:49:52 +0200

    Refactor initial data creation.

--- examples/twitter-app/grails-app/init/grails/plugins/elasticsearch/twitter/BootStrap.groovy
+++ examples/twitter-app/grails-app/init/grails/plugins/elasticsearch/twitter/BootStrap.groovy
@@ -1,42 +1,48 @@
 package grails.plugins.elasticsearch.twitter
 
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
 
     def init = {
-        User.withTransaction {
-            new User(lastname: 'Smith', firstname: 'Alexander').save(failOnError: true)
-            new User(lastname: 'Doe', firstname: 'John').save(failOnError: true)
-            new User(lastname: 'Doe', firstname: 'Jane').save(failOnError: true)
-        }
-        Tag.withTransaction {
-            new Tag(name: 'location').save(failOnError: true)
-            new Tag(name: 'question').save(failOnError: true)
-            new Tag(name: 'information').save(failOnError: true)
-            new Tag(name: 'answer').save(failOnError: true)
-        }
+        createUsersAndTags()
+        createExampleTweets()
+    }
+
+    @Transactional
+    private void createExampleTweets() {
         def alex = User.findByFirstname('Alexander')
         def john = User.findByFirstname('John')
         def jane = User.findByFirstname('Jane')
-        Tweet.withTransaction {
-            new Tweet(user: alex, message: 'Alexander entered the room')
-                    .addToTags(Tag.findByName('location'))
-                    .save(failOnError: true)
-            new Tweet(user: alex, message: 'Alexander left the room')
-                    .addToTags(Tag.findByName('location'))
-                    .save(failOnError: true)
-            new Tweet(user: john, message: 'John entered the building')
-                    .addToTags(Tag.findByName('location'))
-                    .save(failOnError: true)
-            new Tweet(user: jane, message: 'Where is everyone?')
-                    .addToTags(Tag.findByName('question'))
-                    .save(failOnError: true)
-            new Tweet(user: jane, message: 'This example is fun and entertaining.')
-                    .addToTags(Tag.findByName('information'))
-                    .save(failOnError: true)
-            new Tweet(user: jane, message: 'Add some tweets and enjoy!')
-                    .addToTags(Tag.findByName('information'))
-                    .save(failOnError: true)
-        }
+        new Tweet(user: alex, message: 'Alexander entered the room')
+                .addToTags(Tag.findByName('location'))
+                .save(failOnError: true)
+        new Tweet(user: alex, message: 'Alexander left the room')
+                .addToTags(Tag.findByName('location'))
+                .save(failOnError: true)
+        new Tweet(user: john, message: 'John entered the building')
+                .addToTags(Tag.findByName('location'))
+                .save(failOnError: true)
+        new Tweet(user: jane, message: 'Where is everyone?')
+                .addToTags(Tag.findByName('question'))
+                .save(failOnError: true)
+        new Tweet(user: jane, message: 'This example is fun and entertaining.')
+                .addToTags(Tag.findByName('information'))
+                .save(failOnError: true)
+        new Tweet(user: jane, message: 'Add some tweets and enjoy!')
+                .addToTags(Tag.findByName('information'))
+                .save(failOnError: true)
+    }
+
+    @Transactional
+    private void createUsersAndTags() {
+        new User(lastname: 'Smith', firstname: 'Alexander').save(failOnError: true)
+        new User(lastname: 'Doe', firstname: 'John').save(failOnError: true)
+        new User(lastname: 'Doe', firstname: 'Jane').save(failOnError: true)
+        new Tag(name: 'location').save(failOnError: true)
+        new Tag(name: 'question').save(failOnError: true)
+        new Tag(name: 'information').save(failOnError: true)
+        new Tag(name: 'answer').save(failOnError: true)
     }
 
     def destroy = {
